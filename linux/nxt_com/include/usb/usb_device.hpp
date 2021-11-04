77aaa87fd835c00429698835094cf04317593d62
Timon Reich <timon.reich@gmx.de>
2021-11-04 14:30:33 +0100

    Initial check-in of communication library code (see my repo at https://bitbucket.org/schnaggelz/nxt/src/master)

---
+++ linux/nxt_com/include/usb/usb_device.hpp
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (C) 2015 T. Reich
+ *
+ * NXT C++ communication library
+ *
+ * License notes see LICENSE.txt
+ ******************************************************************************/
+
+#ifndef __NXT_COM_USB_DEVICE_HPP__
+#define __NXT_COM_USB_DEVICE_HPP__
+
+#include <libusb-1.0/libusb.h>
+
+#include <cstdint>
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+#define VENDOR_ID 0x0694
+#define PRODUCT_ID 0x0002
+
+#define LIBUSB_ENDPOINT_OUT 0x01
+#define LIBUSB_ENDPOINT_IN 0x82
+#define LIBUSB_RX_TIMEOUT 1000
+
+#define DEFAULT_CMD_ID 42
+#define MAX_CMD_VALUES 4
+#define TX_RX_BYTES MAX_CMD_VALUES * 4 + 2
+
+namespace nxt_com
+{
+struct DataPacket
+{
+    uint16_t id;
+    uint16_t size;
+    int32_t data[8];
+};
+
+static_assert(sizeof(DataPacket) == 36);
+
+class USBDevice
+{
+  public:
+    USBDevice() : m_dev_handle(NULL), m_dev_ready(false){};
+
+    bool init();
+    bool open();
+    void close();
+    void exit();
+
+    inline bool isReady()
+    {
+        return m_dev_ready;
+    };
+
+    void read(DataPacket&);
+    void write(const DataPacket&);
+
+  private:
+    struct libusb_device_handle* m_dev_handle{NULL};
+    bool m_dev_ready{false};
+};
+} // namespace nxt_com
+
+#endif /* __NXT_COM_USB_DEVICE_HPP__ */
---
+++ linux/nxt_com/source/usb_device.cpp
@@ -0,0 +1,149 @@
+/*******************************************************************************
+ * Copyright (C) 2015 T. Reich
+ *
+ * NXT C++ communication library
+ *
+ * License notes see LICENSE.txt
+ ******************************************************************************/
+
+#include "usb/usb_device.hpp"
+
+namespace nxt_com
+{
+
+bool USBDevice::init()
+{
+    return libusb_init(0) == 0;
+}
+
+bool USBDevice::open()
+{
+    int rc = 0;
+
+    libusb_device** dev_list;
+    libusb_device* nxt_dev = NULL;
+
+    ssize_t num_dev = libusb_get_device_list(0, &dev_list);
+
+    for (size_t dev_idx = 0; dev_idx < num_dev; ++dev_idx)
+    {
+        libusb_device* dev = dev_list[dev_idx];
+        libusb_device_descriptor dev_desc = {0};
+
+        rc = libusb_get_device_descriptor(dev, &dev_desc);
+        if (rc >= 0)
+        {
+            if (dev_desc.idVendor == VENDOR_ID &&
+                dev_desc.idProduct == PRODUCT_ID)
+            {
+                nxt_dev = dev;
+            }
+        }
+    }
+
+    libusb_free_device_list(dev_list, 1);
+
+    if (nxt_dev)
+    {
+        rc = libusb_open(nxt_dev, &m_dev_handle);
+        if (rc >= 0 && m_dev_handle != 0)
+        {
+            libusb_detach_kernel_driver(m_dev_handle, 0);
+
+            rc = libusb_set_configuration(m_dev_handle, 1);
+            if (rc >= 0)
+            {
+                rc = libusb_claim_interface(m_dev_handle, 0);
+                if (rc >= 0)
+                {
+                    m_dev_ready = true;
+                    return true;
+                }
+            }
+            else
+            {
+                close();
+            }
+        }
+    }
+
+    return false;
+}
+
+void USBDevice::read(DataPacket& pkg)
+{
+    int rc = 0;
+
+    unsigned char buf[sizeof(pkg)];
+    int nbytes;
+
+    if (m_dev_ready)
+    {
+        rc = libusb_bulk_transfer(m_dev_handle, LIBUSB_ENDPOINT_IN, buf,
+                                  sizeof(buf), &nbytes, LIBUSB_RX_TIMEOUT);
+
+        if (rc == 0 && nbytes >= 4)
+        {
+            pkg.id = ((uint16_t)buf[1] << 8) | (uint8_t)buf[0];
+            pkg.size = ((uint16_t)buf[3] << 8) | (uint8_t)buf[2];
+
+            size_t ndata = (nbytes - 4) / 4;
+
+            for (int i = 0; i < ndata; ++i)
+            {
+                unsigned char* ptr = &(buf[4 * i + 4]);
+                int32_t v = (((int32_t)ptr[3]) << 24) |
+                            (((int32_t)ptr[2]) << 16) |
+                            (((int32_t)ptr[1]) << 8) | (((int32_t)ptr[0]) << 0);
+
+                pkg.data[i] = v;
+            }
+        }
+    }
+}
+
+void USBDevice::write(const DataPacket& pkg)
+{
+    int rc = 0;
+
+    unsigned char buf[sizeof(pkg)];
+    int nbytes;
+
+    if (m_dev_ready)
+    {
+        buf[0] = (pkg.id >> 0) & 0xFF;
+        buf[1] = (pkg.id >> 8) & 0xFF;
+        buf[2] = (pkg.size >> 0) & 0xFF;
+        buf[3] = (pkg.size >> 8) & 0xFF;
+
+        for (int i = 0; i < 2; ++i)
+        {
+            unsigned char* ptr = &(buf[4 * i + 4]);
+            ptr[0] = (pkg.data[i] >> 0) & 0xFF;
+            ptr[1] = (pkg.data[i] >> 16) & 0xFF;
+            ptr[2] = (pkg.data[i] >> 16) & 0xFF;
+            ptr[3] = (pkg.data[i] >> 24) & 0xFF;
+        }
+
+        rc = libusb_bulk_transfer(m_dev_handle, LIBUSB_ENDPOINT_OUT, buf,
+                                  sizeof(buf), &nbytes, LIBUSB_RX_TIMEOUT);
+    }
+}
+
+void USBDevice::close()
+{
+    if (m_dev_ready)
+    {
+        libusb_release_interface(m_dev_handle, 0);
+        libusb_close(m_dev_handle);
+
+        m_dev_ready = false;
+    }
+}
+
+void exit()
+{
+    libusb_exit(0);
+}
+
+} // namespace nxt_com
