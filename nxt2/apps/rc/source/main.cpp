4e950c07492d887ff945003994990aba5db1faeb
Timon Reich <timon.reich@gmx.de>
2021-11-04 15:55:23 +0100

    Make the FW a library

---
+++ nxt2/apps/rc/source/main.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+* Copyright (C) 2015 T. Reich
+*
+* This file is part of rpi-nxt2 experiment.
+*
+* Remote control application. Brick is controlled by a Raspberry Pi
+*
+* License notes see LICENSE.txt
+*******************************************************************************/
+
+
+extern "C" {
+
+void app_bg_task(void){};
+void os_app_init(void){};
+}
