3852a1ce05acc6b36ae64c7575f8212f446cc0c8
Timon Reich <timon.reich@gmx.de>
2021-11-05 14:46:02 +0100

    Add simple logging library

---
+++ linux/simple_logger/include/simple_logger/logger.hpp
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (C) 2015 T. Reich
+ *
+ * Very trivial logger implementation
+ *
+ * License notes see LICENSE.txt
+ ******************************************************************************/
+
+#include <iostream>
+#include <sstream>
+
+#define SIMPLE_LOGGER simple_logger::Logger
+
+#define LOG_LEVEL_INFO simple_logger::LogLevel::INFO
+#define LOG_LEVEL_DEBUG simple_logger::LogLevel::DEBUG
+#define LOG_LEVEL_WARNING simple_logger::LogLevel::WARNING
+#define LOG_LEVEL_ERROR simple_logger::LogLevel::ERROR
+
+#define SLOG_DEBUG SIMPLE_LOGGER(LOG_LEVEL_DEBUG)
+#define SLOG_INFO SIMPLE_LOGGER(LOG_LEVEL_INFO)
+#define SLOG_WARNING SIMPLE_LOGGER(LOG_LEVEL_WARNING)
+#define SLOG_ERROR SIMPLE_LOGGER(LOG_LEVEL_ERROR)
+
+#define LOG_DEBUG(_msg) SIMPLE_LOGGER(LOG_LEVEL_INFO) << _msg;
+#define LOG_INFO(_msg) SIMPLE_LOGGER(LOG_LEVEL_INFO) << _msg;
+#define LOG_WARNING(_msg) SIMPLE_LOGGER(LOG_LEVEL_WARNING) << _msg;
+#define LOG_ERROR(_msg) SIMPLE_LOGGER(LOG_LEVEL_ERROR) << _msg;
+
+namespace simple_logger
+{
+enum class LogLevel
+{
+    ERROR,
+    WARNING,
+    INFO,
+    DEBUG
+};
+
+struct Logger
+{
+    Logger(LogLevel level) : m_level(level){};
+    ~Logger();
+
+    std::stringstream m_os;
+
+  private:
+    LogLevel m_level;
+};
+
+template <typename T> Logger& operator<<(Logger& logger, T&& t)
+{
+    logger.m_os << std::forward<T>(t);
+    return logger;
+}
+
+template <typename T> Logger& operator<<(Logger&& logger, T&& t)
+{
+    return logger << std::forward<T>(t);
+}
+} // namespace SIMPLE_LOGGER
---
+++ linux/simple_logger/source/logger.cpp
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (C) 2015 T. Reich
+ *
+ * Very trivial logger implementation
+ *
+ * License notes see LICENSE.txt
+ ******************************************************************************/
+
+#include "simple_logger/logger.hpp"
+
+namespace simple_logger
+{
+Logger::~Logger()
+{
+    switch (m_level)
+    {
+    case LogLevel::DEBUG:
+        std::cout << "DEBUG_INFO: ";
+        break;
+    case LogLevel::INFO:
+        std::cout << "INFO: ";
+        break;
+    case LogLevel::WARNING:
+        std::cout << "WARNING: ";
+        break;
+    case LogLevel::ERROR:
+        std::cout << "ERROR: ";
+        break;
+    }
+    std::cout << m_os.str() << std::endl;
+}
+} // namespace simple_logger
