99c0cc6ddcd4eecfeb08d45c961d69b441466c8b
Timon Reich <timon.reich@gmx.de>
2021-11-05 14:00:32 +0100

    New base definitions

---
+++ nxt2/libs/app_base/include/base/message_base.hpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (C) 2015 T. Reich
+ *
+ * This file is part of rpi-nxt2 experiment.
+ *
+ * NXT application base definitions
+ *
+ * License notes see LICENSE.txt
+ *******************************************************************************/
+
+#ifndef __NXT_APP_BASE_MESSAGE_BASE_HPP__
+#define __NXT_APP_BASE_MESSAGE_BASE_HPP__
+
+namespace nxt
+{
+namespace app_base
+{
+struct MessageBase
+{
+    template <typename T> static T& instance()
+    {
+        static T _instance;
+        return _instance;
+    }
+};
+
+} // namespace libs
+} // namespace nxt
+
+#endif /* __NXT_APP_BASE_MESSAGE_BASE_HPP__ */
---
+++ nxt2/libs/app_base/include/base/runnable_base.hpp
@@ -0,0 +1,31 @@
+/*******************************************************************************
+* Copyright (C) 2015 T. Reich
+*
+* This file is part of rpi-nxt2 experiment.
+*
+* NXT application base definitions
+*
+* License notes see LICENSE.txt
+*******************************************************************************/
+
+#ifndef __NXT_APP_BASE_RUNNABLE_BASE_HPP__
+#define __NXT_APP_BASE_RUNNABLE_BASE_HPP__
+
+#include <cstdint>
+
+namespace nxt
+{
+namespace app_base
+{
+class Runnable
+{
+public:
+    virtual void init() = 0;
+    virtual void run() = 0;
+    virtual void exit() = 0;
+};
+
+} // shared
+} // nxt
+
+#endif /* __NXT_APP_BASE_RUNNABLE_BASE_HPP__ */
