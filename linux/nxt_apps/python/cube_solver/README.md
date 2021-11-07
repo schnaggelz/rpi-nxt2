357fb55c33746da18511d7179a23bc5871949ec9
Timon Reich <timon.reich@gmx.de>
2021-11-07 16:04:25 +0100

    Python entry point

---
+++ linux/nxt_apps/python/cube_solver/README.md
@@ -0,0 +1,6 @@
+# Python Rubik's Cube Solver
+
+This Python application will use my NXT remote control library with its Python binding `nxt_remote_py` to control the
+Lego model gathered from the MindCuber page (see http://mindcuber.com/).
+
+## HIGHLY_WORK_IN_PROGRESS
---
+++ linux/nxt_apps/python/cube_solver/cube_solver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2021 Timon Reich
+#
+# Released under the MIT license (http://choosealicense.com/licenses/mit/).
+#
+# This Python application will use my NXT remote control library with its Python binding `nxt_remote_py`
+# to control the Lego model gathered from the MindCuber page (see http://mindcuber.com/).
+
+import nxt_remote_py
+
+
+class CubeSolver(object):
+    nxt = nxt_remote_py.Remote()
+
+    def __init__(self, debug=False):
+        self.debug = debug
+
