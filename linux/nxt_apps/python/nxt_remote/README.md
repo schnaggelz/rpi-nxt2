dc06169cdeb9d0d1a61c9423ee9b4a589b05a5ee
Timon Reich <timon.reich@gmx.de>
2021-11-20 15:48:54 +0100

    Some test code

---
+++ linux/nxt_apps/python/nxt_remote/README.md
@@ -0,0 +1,6 @@
+# NXT Remote Library Demos
+
+## `nxt_hello_world.py`
+
+Basic use of all commands to smoke test tha API.
+
---
+++ linux/nxt_apps/python/nxt_remote/nxt_hello_world.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2021 Timon Reich
+#
+# Released under the MIT license (http://choosealicense.com/licenses/mit/).
+#
+# This Python application will use my NXT remote control library with its Python binding `nxt_remote_py`
+# to control the Lego model gathered from the MindCuber page (see http://mindcuber.com/).
+
+import sys
+import logging
+import time
+
+import nxt_remote_py as nxt
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info('Connecting to NXT...')
+r = nxt.Remote()
+
+if not r.connect():
+    logging.error('Could not connect!')
+    sys.exit(1)
+
+logging.warning('Playing a bit with the NXT...')
+
+if not r.motor_fwd(nxt.PORT_A, 50):
+    logging.error('Could not control motor A!')
+    sys.exit(1)
+
+time.sleep(1)
+if not r.motor_rev(nxt.PORT_A, 50):
+    logging.error('Could not control motor A!')
+    sys.exit(1)
+
+time.sleep(1)
+if not r.motor_stop(nxt.PORT_A):
+    logging.error('Could not control motor A!')
+    sys.exit(1)
+
+time.sleep(1)
+if not r.motor_fwd(nxt.PORT_B, 50):
+    logging.error('Could not control motor A!')
+    sys.exit(1)
+
+time.sleep(1)
+if not r.motor_rev(nxt.PORT_B, 50):
+    logging.error('Could not control motor A!')
+    sys.exit(1)
+
+time.sleep(1)
+if not r.motor_stop(nxt.PORT_B):
+    logging.error('Could not control motor A!')
+    sys.exit(1)
+
+time.sleep(1)
+
+logging.info('Disconnecting from NXT...')
+if not r.disconnect():
+    logging.error('Could not disconnect!')
+    sys.exit(1)
+
+logging.info('Done.')
+sys.exit(0)
