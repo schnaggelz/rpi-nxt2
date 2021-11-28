9aff0ba81d9f9e22099e1e38053de73067589e4e
Timon Reich <timon.reich@gmx.de>
2021-11-28 17:11:42 +0100

    Some example code

--- linux/examples/python/nxt_remote/nxt_sampler.py
+++ linux/examples/python/nxt_remote/nxt_sampler.py
@@ -10,14 +10,31 @@
 import sys
 import logging
 import time
+import signal
 
 from nxt_remote_libs import periodic_timer as timer
+from nxt_remote_libs import text_printer as printer
 
 import nxt_remote_py as nxt
 
+stop = False
 
-def hello(name):
-    print("Hello %s!" % name)
+
+def handler(signum, frame):
+    global stop
+    stop = True
+
+
+signal.signal(signal.SIGINT, handler)
+
+p = printer.TextPrinter(sys.stdout)
+
+
+def poll(r):
+    r.poll()
+    p.print_at(10, 2, "M_A: {}".format(str(rc.motor_rcv(nxt.PORT_A, 0))))
+    p.print_at(11, 2, "M_B: {}".format(str(rc.motor_rcv(nxt.PORT_B, 0))))
+    p.print_at(12, 2, "M_C: {}".format(str(rc.motor_rcv(nxt.PORT_C, 0))))
 
 
 logging.basicConfig(level=logging.INFO)
@@ -29,16 +46,23 @@ if not rc.connect():
     logging.error('Could not connect!')
     sys.exit(1)
 
-rt = timer.PeriodicTimer(1, hello, "World")
+rt = timer.PeriodicTimer(1, poll, rc)
+
+if not rc.motor_fwd(nxt.PORT_A, 20):
+    logging.error('Could not control motor A!')
 
 try:
-    time.sleep(5)
+    while not stop:
+        time.sleep(0.1)
 finally:
     rt.stop()
 
+
+if not rc.motor_stop(nxt.PORT_A):
+    logging.error('Could not control motor A!')
+
 if not rc.disconnect():
     logging.error('Could not disconnect!')
-    sys.exit(1)
 
 logging.info('Done.')
 sys.exit(0)
