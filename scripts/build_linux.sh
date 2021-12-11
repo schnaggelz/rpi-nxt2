67e8cb91444230fc62d46a93b52aa5cf36663758
Timon Reich <timon.reich@gmx.de>
2021-12-11 18:23:24 +0100

    Install deployment

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -12,14 +12,15 @@ BUILD_TYPE=$1
 APP_NAME=$2
 
 BUILD_DIR=${ROOT_DIR}/build/linux_${BUILD_TYPE,,}
+INSTALL_DIR=${ROOT_DIR}/install/linux_${BUILD_TYPE,,}
 
 rm -rf "$BUILD_DIR"
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
-# -DCMAKE_TOOLCHAIN_FILE=../cmake/arm-gcc-toolchain
-cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_LINUX=True ${ROOT_DIR}
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_LINUX=True ${ROOT_DIR}
 make clean
 make -j 16 VERBOSE=1
+make install
 
 cd ..
