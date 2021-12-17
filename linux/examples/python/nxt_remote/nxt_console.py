bdb644466d1c07d336ca1db51f25ee754033eb7a
Timon Reich <timon.reich@gmx.de>
2021-12-17 18:54:19 +0100

    Console wip

--- linux/examples/python/nxt_remote/nxt_console.py
+++ linux/examples/python/nxt_remote/nxt_console.py
@@ -7,18 +7,15 @@
 # This Python application will use my NXT remote control library with its Python binding `nxt_remote_py`
 # to control the Lego model gathered from the MindCuber page (see http://mindcuber.com/).
 
-import sys
 import time
 import signal
 
-from nxt_remote_libs import periodic_timer as timer
-from nxt_remote_libs import console_window as console
+from nxt_utils import console_window as console, periodic_timer as timer
 
 import nxt_remote_py as nxt
 
 
 class RemoteConsole:
-
     VERSION = 1
 
     def __init__(self):
@@ -27,30 +24,24 @@ class RemoteConsole:
         self._control = None
         self._stop = False
         self._counter = 0
-        #self._window.print_at(2, 2, "Remote console V{:d}", RemoteConsole.VERSION)
+        self._window.print_at(1, 1, "Remote console V{:d}".format(RemoteConsole.VERSION))
 
     def periodic(self):
-        self._window.print_at(8, 2, "Cycle {:6d}".format(self._counter))
+        self._window.print_at(8, 1, "Cycle {:5d}".format(self._counter))
         self._control.poll()
-        self._window.print_at(10, 2, "M_A: {:6d}".format(self._control.motor_rcv(nxt.PORT_A, 0)))
-        self._window.print_at(11, 2, "M_B: {:6d}".format(self._control.motor_rcv(nxt.PORT_B, 0)))
-        self._window.print_at(12, 2, "M_C: {:6d}".format(self._control.motor_rcv(nxt.PORT_C, 0)))
-        self._window.print_at(13, 2, "S_1: {:6d}".format(self._control.sensor_rcv(nxt.PORT_1, 0)))
-        self._window.print_at(14, 2, "S_2: {:6d}".format(self._control.sensor_rcv(nxt.PORT_2, 0)))
-        self._window.print_at(15, 2, "S_3: {:6d}".format(self._control.sensor_rcv(nxt.PORT_3, 0)))
-        self._window.print_at(16, 2, "S_4: {:6d}".format(self._control.sensor_rcv(nxt.PORT_4, 0)))
+        self.display()
         self._counter += 1
 
     def connect(self):
-        self._window.print_status_at(4, 2, "Connecting to NXT...")
+        self._window.print_status_at(4, 1, "Connecting to NXT...")
         self._control = nxt.Remote()
         if not self._control.connect():
-            self._window.print_error_at(5, 2, "Could not connect!")
+            self._window.print_error_at(5, 1, "Could not connect!")
         return self._control.connected()
 
     def disconnect(self):
         if not self._control.disconnect():
-            self._window.print_error_at(5, 2, "Could not disconnect!")
+            self._window.print_error_at(5, 1, "Could not disconnect!")
 
     def start(self):
         self._timer = timer.PeriodicTimer(0.1, self.periodic)
@@ -61,10 +52,26 @@ class RemoteConsole:
 
             if ch == ord('q'):
                 break  # Exit the while loop
+            elif ch == ord('f'):
+                ch = self._window.get_char()
+                if ch == ord('a'):
+                    break
 
     def stop(self):
         self._stop = True
         self._timer.stop()
+
+    def display(self):
+        self._window.print_at(10, 1, "M_A(0): {:5d}".format(self._control.motor_rcv(nxt.PORT_A, 0)))
+        self._window.print_at(11, 1, "M_B(0): {:5d}".format(self._control.motor_rcv(nxt.PORT_B, 0)))
+        self._window.print_at(12, 1, "M_C(0): {:5d}".format(self._control.motor_rcv(nxt.PORT_C, 0)))
+        self._window.print_at(10, 21, "M_A(1): {:5d}".format(self._control.motor_rcv(nxt.PORT_A, 1)))
+        self._window.print_at(11, 21, "M_B(1): {:5d}".format(self._control.motor_rcv(nxt.PORT_B, 1)))
+        self._window.print_at(12, 21, "M_C(1): {:5d}".format(self._control.motor_rcv(nxt.PORT_C, 1)))
+        self._window.print_at(13, 1, "S_1(0): {:5d}".format(self._control.sensor_rcv(nxt.PORT_1, 0)))
+        self._window.print_at(14, 1, "S_2(0): {:5d}".format(self._control.sensor_rcv(nxt.PORT_2, 0)))
+        self._window.print_at(15, 1, "S_3(0): {:5d}".format(self._control.sensor_rcv(nxt.PORT_3, 0)))
+        self._window.print_at(16, 1, "S_4(0): {:5d}".format(self._control.sensor_rcv(nxt.PORT_4, 0)))
 
 
 rc = RemoteConsole()
