3e90dbc29da1c8db2fd30633597ca17ef298f05f
Timon Reich <timon.reich@gmx.de>
2021-12-20 20:07:31 +0100

    Multiple profiles

--- linux/examples/python/cube_solver/cube_colors.py
+++ linux/examples/python/cube_solver/cube_colors.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import cv2
 import numpy as np
 
@@ -5,34 +7,45 @@ import camera_source as cam
 
 
 class CubeColors:
-    COLORS = (
-        # ('red', ([0, 150, 150], [5, 255, 255])),
-        # ('orange', ([6, 200, 200], [15, 255, 255])),
-        # ('yellow', ([21, 200, 200], [30, 255, 255])),
-        # ('green', ([31, 150, 155], [75, 255, 255])),
-        # ('blue', ([76, 100, 100], [135, 255, 255])),
-        ('purple', ([136, 165, 100], [135, 255, 255])),
-        # ('red', ([166, 150, 150], [180, 255, 255])),
-        ('white', ([0, 0, 200], [179, 55, 255]))
+    PROFILE_COLORS = (
+        ('red', [1, 2], ([0, 100, 150], [10, 255, 255])),
+        ('orange', [2], ([11, 100, 150], [20, 255, 255])),
+        ('yellow', [1, 2], ([21, 100, 150], [35, 255, 255])),
+        ('citron', [2], ([36, 100, 150], [50, 255, 255])),
+        ('green', [1], ([51, 100, 100], [75, 255, 255])),
+        ('cyan', [2], ([76, 100, 100], [95, 255, 255])),
+        # ('ocean', [], ([96, 100, 100], [100, 255, 255])),
+        ('blue', [1], ([96, 100, 100], [120, 255, 255])),
+        ('violet', [1], ([121, 50, 50], [145, 255, 255])),
+        ('magenta', [1], ([146, 100, 50], [165, 255, 255])),
+        # ('raspberry', [], ([166, 100, 50], [170, 255, 255])),
+        ('red', [1, 2], ([171, 150, 150], [180, 255, 255])),
+        ('white', [2], ([0, 0, 200], [179, 55, 255]))
     )
+
+    PROFILES = {
+        'original': 1,
+        'niko': 2
+    }
 
     @staticmethod
     def range(color):
         r = []
-        for entry in CubeColors.COLORS:
+        for entry in CubeColors.PROFILE_COLORS:
             if entry[0] == color:
                 r.append(entry[1])
         return r
 
     @staticmethod
-    def ranges():
-        r = {}
-        for entry in CubeColors.COLORS:
+    def ranges(profile):
+        pix = CubeColors.PROFILES[profile]
+        results = defaultdict(list)
+        for entry in CubeColors.PROFILE_COLORS:
             color = entry[0]
-            if color not in r:
-                r[color] = []
-            r[color].append(entry[1])
-        return r
+            switch = entry[1]
+            if pix in switch:
+                results[color].append(entry[2])
+        return results
 
     @staticmethod
     def print():
