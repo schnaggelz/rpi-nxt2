228f996128e2894db46c3879d9c9765926269efc
Timon Reich <timon.reich@gmx.de>
2021-12-20 15:08:08 +0100

    Get contours by color

--- linux/examples/python/cube_solver/frame_detector.py
+++ linux/examples/python/cube_solver/frame_detector.py
@@ -6,59 +6,72 @@
 #
 # This Python application will use my NXT remote control library with its Python binding `nxt_remote_py`
 # to control the Lego model gathered from the MindCuber page (see http://mindcuber.com/).
+import time
+from collections import defaultdict
 
 import camera_source as cs
+import cube_colors as cc
 import numpy as np
-from imutils import contours
 import cv2
 
 
 class FrameDetector:
-    COLORS = {
-        'gray': ([76, 0, 41], [179, 255, 70]),  # Gray
-        'blue': ([69, 120, 100], [179, 255, 255]),  # Blue
-        'yellow': ([21, 110, 117], [45, 255, 255]),  # Yellow
-        'orange': ([0, 110, 125], [17, 255, 255])  # Orange
-    }
-
     def __init__(self, source_path):
         self.camera = cs.CameraSource(source_path, 640, 480)
         self.camera.open()
 
+    @staticmethod
+    def contours(hsv_frame):
+        color_ctrs = defaultdict(list)
+        for color, ranges in cc.CubeColors.ranges().items():
+            mask = None
+            for range in ranges:
+                r_min = np.array(range[0], np.uint8)
+                r_max = np.array(range[1], np.uint8)
+                if mask is None:
+                    mask = cv2.inRange(hsv_frame, r_min, r_max)
+                else:
+                    mask |= cv2.inRange(hsv_frame, r_min, r_max)
+
+            canny = cv2.Canny(mask, 50, 100)
+            ctrs, h = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+            for ctr in ctrs:
+                _, _, w, h = cv2.boundingRect(ctr)
+                if w > 100 and h > 100:
+                    color_ctrs[color].append(ctr)
+
+        return color_ctrs
+
     def detect(self):
-        image = self.camera.read()
-        if image is None or self.camera.exited():
+        frame = self.camera.read()
+        if frame is None or self.camera.exited():
             return False
 
-        original = image.copy()
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        mask = np.zeros(image.shape, dtype=np.uint8)
+        bf_frame = cv2.bilateralFilter(frame, 9, 75, 75)
+        hsv_frame = cv2.cvtColor(bf_frame, cv2.COLOR_BGR2HSV)
 
-        open_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-        close_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-        for color, (lower, upper) in FrameDetector.COLORS.items():
-            lower = np.array(lower, dtype=np.uint8)
-            upper = np.array(upper, dtype=np.uint8)
-            color_mask = cv2.inRange(image, lower, upper)
-            color_mask = cv2.morphologyEx(color_mask, cv2.MORPH_OPEN, open_kernel, iterations=1)
-            color_mask = cv2.morphologyEx(color_mask, cv2.MORPH_CLOSE, close_kernel, iterations=5)
+        cntrs = self.contours(hsv_frame)
 
-            color_mask = cv2.merge([color_mask, color_mask, color_mask])
-            mask = cv2.bitwise_or(mask, color_mask)
+        for color, cntrs in cntrs.items():
+            for cntr in cntrs:
+                x, y, w, h = cv2.boundingRect(cntr)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                frame = cv2.putText(frame, color, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
+                                    0.5, (0, 0, 0), 1, cv2.LINE_AA)
 
-        cv2.imshow('mask', mask)
+        cv2.imshow("solver", frame)
 
         return True
 
 
 if __name__ == '__main__':
-    address = "http://192.168.242.105:4747/video"
-    address = "/home/timon/Pictures/vlcsnap-2021-12-19-13h26m32s813.png"
+    address = "/dev/video0"
+    # address = "/home/timon/Pictures/vlcsnap.png"
     fd = FrameDetector(address)
 
-    # while True:
-    res = fd.detect()
-        # if not res:
-        #     break
+    while True:
+        res = fd.detect()
+        if not res:
+            break
 
-    cv2.waitKey()
+    # cv2.waitKey()
