e7056265b4884df4ac1fc95c42a84732714da689
Timon Reich <timon.reich@gmx.de>
2022-01-05 19:50:41 +0100

    Bit more wrapping

--- linux/libs/python/cam_utils/pi_camera.py
+++ linux/libs/python/cam_utils/pi_camera.py
@@ -5,35 +5,50 @@
 # Utility code
 #
 
+import time
+
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 
+
 class Camera:
-    def __init__(self, image_width, image_height, read_callback=None):
+    def __init__(self, image_width, image_height, framerate=30, read_callback=None):
         self._image_width = image_width
         self._image_height = image_height
+        self._framerate = framerate
         self._callback = read_callback
 
     def open(self):
-        self._camera = PiCamera()
-        self._camera.resolution = (self._image_width, self._image_height)
+        time.sleep(0.5)
+
+        self._camera = PiCamera(resolution=(self._image_width, self._image_height),
+                                framerate=self._framerate)
+        #self._camera.exposure_mode = 'night'
         self._buffer = PiRGBArray(self._camera,
                                   size=(self._image_width, self._image_height))
+
+    def close(self):
+        if not self.is_open():
+            return
+
+        self._camera.close()
 
     def run(self):
         if not self.is_open():
             return
 
         for frame in self._camera.capture_continuous(self._buffer,
-                                                    format='bgr',
-                                                    use_video_port=True):
+                                                     format='bgr',
+                                                     use_video_port=True):
             if self._callback:
                 self._callback(frame.array)
 
             self._buffer.truncate(0)
 
     def is_open(self):
-        return self._camera is not None
+        if self._camera is not None:
+            return not self._camera.closed
+        return False
 
     def read(self):
         if not self.is_open():
@@ -48,13 +63,16 @@ class Camera:
 
 
 if __name__ == '__main__':
-
     def receive(image):
         pass
 
-        #print('Captured %dx%d image' % (
+        # print('Captured %dx%d image' % (
         #    image.shape[1], image.shape[0]))
 
-    camera = Camera(640, 480, receive)
+
+    camera = Camera(image_width=640,
+                    image_height=480,
+                    framerate=10,
+                    read_callback=receive)
     camera.open()
     camera.run()
