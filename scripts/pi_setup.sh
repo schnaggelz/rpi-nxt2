8e51fc8f90537c50a9fb62cd439b8dafb0be7ce2
Timon Reich <timon.reich@gmx.de>
2022-01-05 19:53:39 +0100

    Setup script

---
+++ scripts/pi_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname $(readlink -f "$BASH_SOURCE[0]"))
+SOURCE_DIR=$(realpath "${SCRIPT_DIR}/..")
+
+echo "$SOURCE_DIR"
+
+export PYTHONPATH=$SOURCE_DIR/linux/libs/python
