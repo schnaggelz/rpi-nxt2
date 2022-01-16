fcc54bfe0b4dbb34a12b9944dc1a83b7e54bbcc5
Timon Reich <timon.reich@gmx.de>
2022-01-16 19:54:31 +0100

    Make detector a separate process to parallelize more

--- linux/examples/python/cube_solver/cube_solver.py
+++ linux/examples/python/cube_solver/cube_solver.py
@@ -9,9 +9,15 @@
 
 import time
 import signal
+import queue
+
+from multiprocessing import Process, Queue
+from threading import Thread
 
 from common_utils.console_window import ConsoleWindow
 from common_utils.periodic_timer import PeriodicTimer
+
+from vision_utils.video_sender import VideoSender
 
 from solver_machine import SolverMachine
 from color_detector import ColorDetector
@@ -20,109 +26,152 @@ from color_detector import ColorDetector
 class CubeSolver:
     VERSION = 1
 
+    class DetectorReceiver(Thread):
+
+        def __init__(self, queue, callback):
+            super().__init__()
+            self.__queue = queue
+            self.__callback = callback
+            self.__stop = False
+
+        def run(self):
+            while not self.__stop:
+                try:
+                    value = self.__queue.get(timeout=1)
+                    self.__callback(value)
+                except queue.Empty:
+                    pass
+
+        def stop(self):
+            self.__stop = True
+
     def __init__(self):
-        self._window = ConsoleWindow()
-        self._machine = SolverMachine()
-        self._detector = ColorDetector()
+        self.__console_window = ConsoleWindow()
+        self.__solver_machine = SolverMachine()
 
-        self._timer = PeriodicTimer(0.1, self.display)
-        self._stop = False
-        self._window.print_at(1, 1, "Remote console V{:d}".format(self.VERSION))
+        self.__received_patterns = 0
+        self.__current_pattern = ''
+        self.__patterns = Queue(maxsize=10)
 
-    def __del__(self):
-        self.stop()
+        self.__detector_process = Process(target=CubeSolver.run_detector,
+                                          args=(self.__patterns,))
 
-    @property
-    def stopped(self):
-        return self._stop
+        self.__detector_receiver = self.DetectorReceiver(
+            queue=self.__patterns,
+            callback=self.check_pattern)
+
+        self.__timer = PeriodicTimer(0.1, self.display)
 
     def init(self):
-        if self._machine.connect():
-            self._window.print_status_at(5, 1, "Connected to NXT")
+        if self.__solver_machine.connect():
+            self.__console_window.print_status_at(3, 1, "Connected to NXT")
         else:
-            self._window.print_error_at(5, 1, "Could not connect!")
+            self.__console_window.print_error_at(3, 1, "Could not connect!")
 
     def exit(self):
-        if self._machine.disconnect():
-            self._window.print_status_at(5, 1, "Disconnected from NXT")
-        else:
-            self._window.print_error_at(5, 1, "Could not disconnect!")
+        self.__solver_machine.disconnect()
 
     def start(self):
-        self._detector.start()
-        self._machine.start()
-        self._window.print_status_at(6, 1, "Running")
+        self.__detector_process.start()
+        self.__detector_receiver.start()
+        self.__solver_machine.start()
+        self.__console_window.print_status_at(4, 1, "RUNNING!")
 
-        self._stop = False
-        while not self._stop:
+        while True:
             time.sleep(0.5)
-            ch = self._window.get_char()
+            ch = self.__console_window.get_char()
             if ch == ord('h'):
-                self._machine.turntable_home()
+                self.__solver_machine.turntable_home()
             if ch == ord('e'):
-                self._machine.turntable_turn_ccw()
+                self.__solver_machine.turntable_turn_ccw()
             if ch == ord('w'):
-                self._machine.turntable_turn_cw()
+                self.__solver_machine.turntable_turn_cw()
             if ch == ord('z'):
-                self._machine.turntable_home(True)
+                self.__solver_machine.turntable_home(True)
             if ch == ord('x'):
-                self._machine.turntable_turn_ccw(True)
+                self.__solver_machine.turntable_turn_ccw(True)
             if ch == ord('y'):
-                self._machine.turntable_turn_cw(True)
+                self.__solver_machine.turntable_turn_cw(True)
             if ch == ord('o'):
-                self._machine.grabber_home()
+                self.__solver_machine.grabber_home()
             if ch == ord('r'):
-                self._machine.grabber_rest()
+                self.__solver_machine.grabber_rest()
             if ch == ord('g'):
-                self._machine.grabber_grab()
+                self.__solver_machine.grabber_grab()
             if ch == ord('f'):
-                self._machine.grabber_flip()
+                self.__solver_machine.grabber_flip()
             if ch == ord('c'):
-                self._machine.scanner_scan()
+                self.__solver_machine.scanner_scan()
             if ch == ord('m'):
-                self._machine.scanner_home()
+                self.__solver_machine.scanner_home()
             if ch == ord('s'):
                 self.scan_all_colors()
             elif ch == ord('q'):
-                self.stop()
+                break
+
+        self.__console_window.print_status_at(4, 1, "STOP!")
+
+        self.stop()
 
     def stop(self):
-        self._window.print_status_at(5, 1, "STOP!")
-        self._detector.stop()
-        self._machine.stop()
-        self._timer.stop()
-        self._stop = True
+        self.__detector_receiver.stop()
+        self.__detector_receiver.join(2)
+
+        self.__detector_process.terminate()
+        self.__detector_process.join(2)
+
+        self.__solver_machine.stop()
+        self.__timer.stop()
 
     def display(self):
-        self._window.print_at(10, 1, "MA: {:5d}".format(self._machine.decoder_values[0]))
-        self._window.print_at(11, 1, "MB: {:5d}".format(self._machine.decoder_values[1]))
-        self._window.print_at(12, 1, "MC: {:5d}".format(self._machine.decoder_values[2]))
-        self._window.print_at(13, 1, "S1: {:5d}".format(self._machine.sensor_values[0]))
-        self._window.print_at(14, 1, "S1: {:5d}".format(self._machine.sensor_values[1]))
-        self._window.print_at(15, 1, "S1: {:5d}".format(self._machine.sensor_values[2]))
-        self._window.print_at(16, 1, "S1: {:5d}".format(self._machine.sensor_values[3]))
-        self._window.print_status_at(5, 1, "PC:{:6d}".format(self._machine.counter))
-        self._window.refresh()
+        self.__console_window.print_at(1, 1, "CUBER V{:d}".format(self.VERSION))
+
+        self.__console_window.print_at(5, 1, "CONTROL:  {:5d}".format(self.__solver_machine.counter))
+        self.__console_window.print_at(7, 1, "RECEIVED: {:5d}".format(self.__received_patterns))
+
+        self.__console_window.print_at(8, 1, "CURRENT PATTERN: '{}'".format(self.__current_pattern))
+
+        self.__console_window.print_at(10, 1, "MA: {:5d}".format(self.__solver_machine.decoder_values[0]))
+        self.__console_window.print_at(11, 1, "MB: {:5d}".format(self.__solver_machine.decoder_values[1]))
+        self.__console_window.print_at(12, 1, "MC: {:5d}".format(self.__solver_machine.decoder_values[2]))
+        self.__console_window.print_at(13, 1, "S1: {:5d}".format(self.__solver_machine.sensor_values[0]))
+        self.__console_window.print_at(14, 1, "S1: {:5d}".format(self.__solver_machine.sensor_values[1]))
+        self.__console_window.print_at(15, 1, "S1: {:5d}".format(self.__solver_machine.sensor_values[2]))
+        self.__console_window.print_at(16, 1, "S1: {:5d}".format(self.__solver_machine.sensor_values[3]))
+
+        self.__console_window.refresh()
 
     def home(self):
-        self._machine.scanner_home()
-        self._machine.turntable_home()
-        self._machine.grabber_home()
+        self.__solver_machine.scanner_home()
+        self.__solver_machine.turntable_home()
+        self.__solver_machine.grabber_home()
 
     def flip_cube(self):
-        self._machine.grabber_grab()
-        self._machine.grabber_flip()
+        self.__solver_machine.grabber_grab()
+        self.__solver_machine.grabber_flip()
 
     def turn_cube(self, ccw=False):
         if ccw:
-            self._machine.turntable_turn_ccw()
+            self.__solver_machine.turntable_turn_ccw()
         else:
-            self._machine.turntable_turn_cw()
+            self.__solver_machine.turntable_turn_cw()
 
     def scan_colors(self, side):
-        self._machine.scanner_scan()
-        time.sleep(1)  # mimic
-        self._machine.scanner_home()
+        self.__solver_machine.scanner_scan()
+
+        time.sleep(1)
+
+        # patterns = self.__detector.patterns
+        # if len(patterns) > 0:
+        #     self.__current_pattern = self.__detector.patterns[0]
+
+        # if countdown > 0:
+        #     occurrences = collections.Counter(self.__detector.patterns)
+        #     self.__current_pattern = occurrences.most_common(1)[0][0]
+        # else:
+        #     self.__current_pattern = 'NONE'
+
+        self.__solver_machine.scanner_home()
 
     def scan_all_colors(self):
         self.home()
@@ -140,12 +189,26 @@ class CubeSolver:
         self.scan_colors('left')
         self.home()
 
+    def check_pattern(self, pattern):
+        self.__current_pattern = pattern
+        self.__received_patterns += 1
+
+    def run_detector(queue):
+        video_sink = VideoSender('192.168.242.137', 4243)  # configurable?
+        video_sink.connect()
+
+        detector = ColorDetector(output=queue)
+        detector.video_sink = video_sink
+        detector.start()
+
 
 if __name__ == '__main__':
     cbr = CubeSolver()
 
+
     def handler(signum, frame):
         cbr.stop()
+
 
     signal.signal(signal.SIGINT, handler)
 
