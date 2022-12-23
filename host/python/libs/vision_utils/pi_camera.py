6ce040876baf890e0d2fee1cce2c6b2329a96445
Timon Reich <timon.reich@gmx.de>
2022-12-23 21:16:13 +0100

    ported camera wrapper to picamera2

--- host/python/libs/vision_utils/pi_camera.py
+++ host/python/libs/vision_utils/pi_camera.py
@@ -10,106 +10,97 @@ import numpy as np
 import threading
 import picamera2 as picam2
 
-from picamera2 import encoders as cam_encoders
-from picamera2 import outputs as cam_outputs
 
 class Camera(threading.Thread):
 
     def __init__(self, width=1640, height=1232, framerate=30, callback=None):
         super().__init__()
-        self.__size = (width, height)
-        self.__lores_size = (320, 240)
-        self.__framerate = framerate
-        self.__callback = callback
-        self.__processor = None
-        self.__stop_flag = False
+        self._camera = None
+        self._main_size = (width, height)
+        self._lores_size = (320, 240)
+        self._framerate = framerate
+        self._callback = callback
+        self._stop_flag = False
+        self._is_open = False
 
-    def open(self, lsize = (320, 240)):
-        time.sleep(0.5)
+    def open(self):
+        self._camera = picam2.Picamera2()
+        video_config = self._camera.create_video_configuration(
+            main={"size": self._main_size, "format": "XRGB8888"},
+            lores={"size": self._lores_size, "format": "YUV420"})
 
-        self.__camera = picam2.PiCamera2()
-        video_config = self.__camera.create_video_configuration(
-            main={"size": self.__size, "format": "XRGB8888"},
-            lores={"size": self.__lores_size, "format": "YUV420"})
-        self.__camera.configure(video_config)
-
-        encoder = cam_encoders.H264Encoder(1000000, repeat=True)
-        output = cam_outputs.CircularOutput()
-        encoder.output = [output]
-
-        self.__camera.encoder = encoder
-
+        self._camera.configure(video_config)
+        self._is_open = True
 
     def close(self):
         if not self.is_open():
             return
 
-        self.__camera.close()
+        self._camera.close()
+        self._is_open = False
+
+    def is_open(self):
+        if self._camera is not None:
+            if self._is_open:
+                return True
+
+        return False
 
     def run(self):
-        self.__camera.start()
-        self.__camera.start_encoder()
-
-        self.__processor = self.FrameProcessor(self.__camera, self.__callback)
+        self._camera.start()
         try:
-            while not self.__stop_flag:
-                self.__camera.wait_recording(1)
+            while not self._stop_flag:
+                array = self._camera.capture_array()
+                if self._callback:
+                    self._callback(array)
         finally:
-            self.__camera.stop_recording()
+            self._camera.stop()
 
     def start(self):
         if not self.is_open():
             return
 
-        self.__stop_flag = False
+        self._stop_flag = False
 
         super().start()
 
     def stop(self):
-        self.__stop_flag = True
+        self._stop_flag = True
 
         super().join()
-
-    def is_open(self):
-        if self.__camera is not None and self.__processor is not None:
-            return not self.__camera.closed
-
-        return False
 
     def read(self):
         if not self.is_open():
             return
-        
-        array = self.__camera.picam2.capture_array()
 
-        return array
+        return self._camera.capture_array()
 
 
 if __name__ == '__main__':
-    last_time = time.time()
-    counter = 0
+    import fps_calcularor as fps
 
+    counter = 0
+    fps_calc = fps.FpsCalculator()
 
     def receive(image):
-        global last_time
         global counter
 
-        elapsed_time = time.time() - last_time
-        secs_elapsed = elapsed_time % 60
-        fps = 1 / secs_elapsed
-
+        current_fps = fps_calc()
         if counter % 50 == 0:
-            print("{}: {} fps".format(str(image.shape), fps))
+            print("{}: {} fps".format(str(image.shape), current_fps))
 
         counter += 1
-        last_time = time.time()
 
-
-    camera = Camera(width=640,
-                    height=480,
+    camera = Camera(width=1640,
+                    height=1232,
                     framerate=10,
                     callback=receive)
     camera.open()
     camera.start()
 
-    camera.join()
+    try:
+        camera.join()
+    except KeyboardInterrupt:
+        camera.stop()
+    
+    camera.close()
