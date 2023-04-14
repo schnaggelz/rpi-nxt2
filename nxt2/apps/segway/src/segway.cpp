60011388ccc015678dec27e7aa09dafa8e72d32e
Timon Reich <timon.reich@gmx.de>
2023-04-14 13:58:02 +0200

    Impl wip

--- nxt2/apps/segway/src/segway.cpp
+++ nxt2/apps/segway/src/segway.cpp
@@ -10,6 +10,8 @@
 
 #include "segway.hpp"
 
+#include "utils/maths.h"
+
 #include "nxt/nxt_system.hpp"
 
 namespace nxt
@@ -19,12 +21,20 @@ namespace segway
 void Segway::init()
 {
     _gyro_sensor.init();
+    _gyro_sensor.setMode(nxt::fw::ht::GyroSensorMode::PREVIEW);
+
+    nxt::fw::system::beep(500);
+
     _motor_angle_index = 0;
 
-    for(auto i = 0; i < NUM_CALIBRATION_CYCLES; i++)
+    for (auto i = 0; i < NUM_CALIBRATION_CYCLES; i++)
     {
-        _gyro_offset += (_gyro_sensor.getAnglarVelocity() - APPROX_SENSOR_OFFSET) * SENSOR_RESOLUTION;
-        
+        _gyro_sensor.read();
+        _gyro_sensor_raw_value = _gyro_sensor.getAnglarVelocity();
+
+        _gyro_offset +=
+            (_gyro_sensor_raw_value - APPROX_SENSOR_OFFSET) * SENSOR_RESOLUTION;
+
         nxt::fw::system::wait(2);
     }
 
@@ -33,11 +43,59 @@ void Segway::init()
     nxt::fw::system::beep(500);
 }
 
-void Segway::step() {}
+void Segway::sense()
+{
+    _gyro_sensor.read();
+    _gyro_sensor_raw_value = _gyro_sensor.getAnglarVelocity();
+}
 
-void Segway::exit() 
+void Segway::step()
+{
+    // Get gyro rate
+    _gyro_sensor.read();
+    _gyro_sensor_raw_value = _gyro_sensor.getAnglarVelocity();
+
+    _gyro_rate =
+        (_gyro_sensor_raw_value - APPROX_SENSOR_OFFSET) * SENSOR_RESOLUTION -
+        _gyro_offset;
+
+    // Get motor position
+    auto motor_angle = -_motor.getCurrentCount();
+    _motor_angle_reference = 0;  // update value here
+    _motor_angle_error = motor_angle - _motor_angle_reference;
+
+    // Compute motor speed
+    _motor_angular_speed =
+        (motor_angle - _motor_angle_history[_motor_angle_index]);
+    _motor_angular_speed_error = _motor_angular_speed;
+    _motor_angle_history[_motor_angle_index] = motor_angle;
+
+    // Compute motor duty cycle
+    _motor_duty_cycle =
+        (GYRO_ANGLE_GAIN * _gyro_angle_estimated +
+         (GYRO_ANGLE_GAIN * _gyro_rate) / 8 +
+         MOTOR_ANGLE_GAIN * _motor_angle_error +
+         MOTOR_ANGULAR_SPEED_GAIN * _motor_angular_speed_error +
+         MOTOR_ANGLE_ERROR_ACCUM_GAIN * _motor_angle_error_accum);
+
+    _motor_duty_cycle = _motor_duty_cycle + sign(_motor_duty_cycle) * 5;
+
+    // Control motors
+    // TODO
+
+    // Update angle estimate and gyro offset estimate
+    _gyro_angle_estimated =
+        _gyro_angle_estimated + _gyro_rate / SENSOR_RESOLUTION;
+    _gyro_offset = 0;
+
+    // Update accumulated motor error
+    _motor_angle_error_accum = _motor_angle_error_accum + _motor_angle_error;
+    _motor_angle_index = (_motor_angle_index + 1) % _motor_angle_history.size();
+}
+
+void Segway::exit()
 {
     _gyro_sensor.exit();
 }
-} // namespace segway
-} // namespace nxt
+}  // namespace segway
+}  // namespace nxt
--- nxt2/apps/segway/src/segway.hpp
+++ nxt2/apps/segway/src/segway.hpp
@@ -24,12 +24,11 @@ namespace segway
 {
 class Segway
 {
-  public:
+   public:
     Segway(std::uint16_t sample_period_ms)
-        : _sample_period_ms(sample_period_ms)
-        , _motors({nxt::fw::Motor(nxt::fw::Actuator::Port::PORT_A),
-                   nxt::fw::Motor(nxt::fw::Actuator::Port::PORT_B)})
-        , _gyro_sensor(nxt::fw::Sensor::Port::PORT_1)
+        : _motor(nxt::fw::Actuator::Port::PORT_A)
+        , _gyro_sensor(nxt::fw::Sensor::Port::PORT_2)
+        , _sample_period_ms(sample_period_ms)
     {
     }
 
@@ -38,6 +37,7 @@ class Segway
     void init();
     void step();
     void exit();
+    void sense();
 
     void setSpeed(std::uint16_t speed)
     {
@@ -49,15 +49,34 @@ class Segway
         _direction = direction;
     }
 
+    std::int32_t getMotorDutyCycle()
+    {
+        return _motor_duty_cycle;
+    }
+
+    std::int32_t getGyroSensorRawValue()
+    {
+        return _gyro_sensor_raw_value;
+    }
+
+    std::int32_t getGyroRate()
+    {
+        return _gyro_rate;
+    }
+
     static constexpr std::uint16_t SENSOR_RESOLUTION = 16;
     static constexpr std::uint16_t APPROX_SENSOR_OFFSET = 585;
     static constexpr std::uint16_t NUM_CALIBRATION_CYCLES = 50;
+    static constexpr std::uint16_t GYRO_ANGLE_GAIN = 45;
+    static constexpr std::uint16_t MOTOR_ANGLE_GAIN = 200;
+    static constexpr std::uint16_t MOTOR_ANGULAR_SPEED_GAIN = 500;
+    static constexpr std::uint16_t MOTOR_ANGLE_ERROR_ACCUM_GAIN = 0;
 
-  private:
-    std::uint16_t _sample_period_ms;
-    std::array<nxt::fw::Motor, 2> _motors;
+   //private:
+    nxt::fw::Motor _motor;
     nxt::fw::ht::GyroSensor _gyro_sensor;
 
+    std::uint16_t _sample_period_ms;
     std::uint16_t _speed{0};
     std::uint16_t _direction{0};
 
@@ -71,12 +90,13 @@ class Segway
     std::int32_t _motor_angular_speed_error{0};
     std::int32_t _motor_duty_cycle{0};
 
+    std::int32_t _gyro_sensor_raw_value{0};
     std::int32_t _gyro_rate{0};
     std::int32_t _gyro_offset{0};
     std::int32_t _gyro_angle_estimated{0};
 };
 
-} // namespace segway
-} // namespace nxt
+}  // namespace segway
+}  // namespace nxt
 
 #endif /* __NXT_RC_SEGWAY_HPP__ */
