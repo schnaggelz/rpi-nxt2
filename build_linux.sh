723fa03a0c9adf6a543f0838406fcb6f2aed42d2
Timon Reich <timon.reich@gmx.de>
2025-11-15 16:15:10 +0100

    feat: install linux binaries

--- build_linux.sh
+++ build_linux.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
-ROOT_DIR=$(realpath "$SCRIPT_DIR/..")
+ROOT_DIR=$(realpath "$SCRIPT_DIR")
 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
   then
-    echo "No parameters specified! Need <target_name> <build_type>"
+    echo "No parameters specified! Need <build_type>"
     exit 1
 fi
 
-TARGET_NAME=$1
-BUILD_TYPE=$2
+BUILD_TYPE=$1
 
-BUILD_DIR=${ROOT_DIR}/build/${TARGET_NAME}-linux-${BUILD_TYPE,,}
-INSTALL_DIR=${HOME}/.local
+BUILD_DIR=$ROOT_DIR/build/linux-${BUILD_TYPE,,}
+INSTALL_DIR=$ROOT_DIR/install/linux-${BUILD_TYPE,,}
 
-rm -rf "$BUILD_DIR"
-mkdir -p "$BUILD_DIR"
-cd "$BUILD_DIR"
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 
-cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_LINUX=True ${ROOT_DIR}/src
-make clean
-make -j 16 VERBOSE=1
-make install
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_LINUX=True $ROOT_DIR/src
+cmake --build $BUILD_DIR
+cmake --install $BUILD_DIR --prefix $INSTALL_DIR
 
 cd ..
