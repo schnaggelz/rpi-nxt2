3ed88b2482e5f31e1b2d83973e2646b52d21caf6
Timon Reich <timon.reich@gmx.de>
2025-11-22 16:58:53 +0100

    feat: ROS2 build script

---
+++ build_ros.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+ROOT_DIR=$(realpath "$SCRIPT_DIR")
+
+if [ $# -ne 1 ]
+  then
+    echo "No parameters specified! Need <build_type>"
+    exit 1
+fi
+
+BUILD_TYPE=$1
+TARGET_NAME=ros2
+
+BUILD_DIR=$ROOT_DIR/build/${TARGET_NAME}-${BUILD_TYPE,,}
+INSTALL_DIR=$ROOT_DIR/install/${TARGET_NAME}-${BUILD_TYPE,,}
+
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+mkdir -p $INSTALL_DIR
+
+colcon build --base-paths $ROOT_DIR/src/ros2/nxt_drivers/ --build-base $BUILD_DIR --install-base $INSTALL_DIR --cmake-args -DCMAKE_BUILD_TYPE=$BUILD_TYPE
