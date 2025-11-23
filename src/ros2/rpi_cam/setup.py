ae26849955635efd2fbab7e819d30fcce75e4883
Timon Reich <timon.reich@gmx.de>
2025-11-23 11:18:29 +0100

    chore: frame for rpi camera node

---
+++ src/ros2/rpi_cam/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'rpi_cam'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Timon Reich',
+    maintainer_email='timon.reich@gmx.de',
+    description='Raspberry Pi Camera Node',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
